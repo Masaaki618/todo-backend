b4ecb4ff393821542ec238fff285d6ddd7bc1230
Masaaki618 <masaaki_618@icloud.com>
2026-03-14 15:26:53 +0900

    item_serviceの追加

--- services/item_service.go
+++ services/item_service.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"todo-backend/models"
+	"todo-backend/repositories"
+)
+
+type IItemService interface {
+	FindALL() (*[]models.Item, error)
+}
+
+type ItemService struct {
+	service repositories.IItemRepository
+}
+
+func NewItemService(service repositories.IItemRepository) IItemService {
+	return &ItemService{service: service}
+}
+
+func (s *ItemService) FindALL() (*[]models.Item, error) {
+	return s.service.FindAll()
+}
