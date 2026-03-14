33285eb1bcd8f18cdd3fbf9c1c4fbef7c0f5096f
Masaaki618 <masaaki_618@icloud.com>
2026-03-14 15:27:24 +0900

    メモリー動作で動くitem一覧を追加

--- main.go
+++ main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"net/http"
+	"todo-backend/controllers"
+	"todo-backend/models"
+	"todo-backend/repositories"
+	"todo-backend/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// loggerとrecoveryミドルウェア付きGinルーター作成
+	items := []models.Item{
+		{Id: 1, Title: "筋トレ", Done: false},
+		{Id: 2, Title: "サッカー", Done: true},
+		{Id: 3, Title: "スノボ", Done: false},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
 	r := gin.Default()
+	group := r.Group("api/v1/items")
+	group.GET("", itemController.FindAll)
+	r.Run("localhost:8080") // デフォルトで0.0.0.0:8080で待機します
+	// loggerとrecoveryミドルウェア付きGinルーター作成
 
 	// 簡単なGETエンドポイント定義
-	r.GET("/ping", func(c *gin.Context) {
-		// JSONレスポンスを返す
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong!?",
-		})
-	})
 
 	// ポート8080でサーバー起動（デフォルト）
 	// 0.0.0.0:8080（Windowsではlocalhost:8080）で待機
-	r.Run()
 }
