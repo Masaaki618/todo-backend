45e79fa17b1a4d211c242b222592a098d3083cc3
Masaaki618 <masaaki_618@icloud.com>
2026-03-14 15:27:04 +0900

    item_controllerの追加

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"net/http"
+	"todo-backend/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type IItemController interface {
+	FindAll(c *gin.Context)
+}
+
+type ItemController struct {
+	service services.IItemService
+}
+
+func NewItemController(service services.IItemService) IItemController {
+	return &ItemController{service: service}
+}
+
+func (i *ItemController) FindAll(ctx *gin.Context) {
+	items, err := i.service.FindALL()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid id"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": items})
+}
